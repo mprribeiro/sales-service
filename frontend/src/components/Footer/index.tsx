36cc328df52d6599d1faae6633bf64a60a46c6bf
Marcos Ribeiro <mprribeiro1902@gmail.com>
2021-05-05 20:17:50 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+function Footer() {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/mprribeiro" target="_blank" rel="noreferrer">Marcos Ribeiro</a></p>
+                <p className="text-light"><small><strong>Semana Spring React - Escola DevSuperior</strong><br />
+      Instagram: <a href="https://instagram.com/rabelor_" target="_blank" rel="noreferrer">@rabelor_</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
