055c84bb288f2821d6ebc706696df505d1c1b708
Marcos Ribeiro <mprribeiro1902@gmail.com>
2021-05-07 19:32:05 -0300

    Group by search

--- backend/src/main/java/com/mprribeiro/salesservice/services/SaleService.java
+++ backend/src/main/java/com/mprribeiro/salesservice/services/SaleService.java
@@ -1,5 +1,7 @@
 package com.mprribeiro.salesservice.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.mprribeiro.salesservice.dto.SaleDTO;
+import com.mprribeiro.salesservice.dto.SaleSuccessDTO;
+import com.mprribeiro.salesservice.dto.SaleSumDTO;
 import com.mprribeiro.salesservice.entities.Sale;
 import com.mprribeiro.salesservice.repositories.SaleRepository;
 import com.mprribeiro.salesservice.repositories.SellerRepository;
@@ -25,5 +29,15 @@ public class SaleService {
 		sellerRepository.findAll();
 		Page<Sale> result = repository.findAll(pageable);
 		return result.map(sale -> new SaleDTO(sale));
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller() {
+		return repository.amountGroupedBySeller();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupedBySeller() {
+		return repository.successGroupedBySeller();
 	}
 }
